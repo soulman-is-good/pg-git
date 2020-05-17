a728c5f5c63d97917b11369170e5d842a490fffb
Soul_man <soulman.is.good@gmail.com>
2020-05-18 00:45:03 +0200

    fix mocha type error

--- test/utils.ts
+++ test/utils.ts
@@ -2,6 +2,7 @@ import Docker from 'dockerode';
 import { PGOptions } from '../src/lib/pgOptions';
 
 export interface Container {
+  start(opts?: {}): Promise<unknown>;
   kill(opts?: {}, callback?: Function): Promise<unknown>;
   logs(opts?: {}): Promise<unknown>;
 }
@@ -9,38 +10,36 @@ export interface Container {
 export const runPostgres = (options: Partial<PGOptions>): Promise<Container> => {
   const docker = new Docker();
 
-  return docker
-    .createContainer(
-      {
-        Image: 'postgres:11',
-        name: 'pggittest',
-        AttachStdin: true,
-        AttachStdout: true,
-        AttachStderr: true,
-        Tty: false,
-        OpenStdin: false,
-        StdinOnce: false,
-        HostConfig: {
-          AutoRemove: true,
-          PortBindings: {
-            '5432/tcp': [
-              {
-                HostPort: options.port.toString(),
-              },
-            ],
-          },
+  return (docker.createContainer(
+    {
+      Image: 'postgres:11',
+      name: 'pggittest',
+      AttachStdin: true,
+      AttachStdout: true,
+      AttachStderr: true,
+      Tty: false,
+      OpenStdin: false,
+      StdinOnce: false,
+      HostConfig: {
+        AutoRemove: true,
+        PortBindings: {
+          '5432/tcp': [
+            {
+              HostPort: options.port.toString(),
+            },
+          ],
         },
-        Env: [
-          `POSTGRES_USER=${options.user}`,
-          `POSTGRES_PASSWORD=${options.password}`,
-          `POSTGRES_DB=${options.database}`,
-        ],
       },
-      undefined,
-    )
-    .then(async container => {
-      await container.start();
+      Env: [
+        `POSTGRES_USER=${options.user}`,
+        `POSTGRES_PASSWORD=${options.password}`,
+        `POSTGRES_DB=${options.database}`,
+      ],
+    },
+    undefined,
+  ) as Promise<Container>).then(async container => {
+    await container.start();
 
-      return container;
-    });
+    return container;
+  });
 };
