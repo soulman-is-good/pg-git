b39b2adf3b80d7c739ebcba186f0ac4466327319
Soul_man <soulman.is.good@gmail.com>
2020-05-18 10:03:03 +0200

    StringStream is Duplex

--- src/lib/StringStream.ts
+++ src/lib/StringStream.ts
@@ -1,13 +1,18 @@
-import { Readable } from 'stream';
+import { Duplex, DuplexOptions } from 'stream';
 
-export class StringStream extends Readable {
+export class StringStream extends Duplex {
   private _string: string;
   private _lastReadBype: number;
 
-  constructor(str: string) {
-    super();
-    this._string = str;
+  constructor(str?: string, opts?: DuplexOptions) {
+    super(opts);
+    this._string = str || '';
     this._lastReadBype = 0;
+  }
+
+  _write(chunk: Buffer | string, _encoding: string, callback: (err?: Error) => void) {
+    this._string += chunk instanceof Buffer ? chunk.toString() : chunk;
+    callback();
   }
 
   _read(size: number) {
@@ -17,7 +22,16 @@ export class StringStream extends Readable {
       const len = Math.min(this._lastReadBype + size, this._string.length);
 
       this.push(Buffer.from(this._string.substring(this._lastReadBype, len)));
-      this._lastReadBype = size;
+      this._lastReadBype += size;
     }
+  }
+
+  _final(callback: (err?: Error) => void) {
+    callback();
+    this.emit('close');
+  }
+
+  toString() {
+    return this._string;
   }
 }
