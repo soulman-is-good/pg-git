5a63b88d4347030777373d73b343dcdcb89d2396
Soul_man <soulman.is.good@gmail.com>
2020-05-20 09:22:38 +0200

    Updated dist

--- dist/src/lib/StringStream.d.ts
+++ dist/src/lib/StringStream.d.ts
@@ -1,8 +1,11 @@
 /// <reference types="node" />
-import { Readable } from 'stream';
-export declare class StringStream extends Readable {
+import { Duplex, DuplexOptions } from 'stream';
+export declare class StringStream extends Duplex {
     private _string;
     private _lastReadBype;
-    constructor(str: string);
+    constructor(str?: string, opts?: DuplexOptions);
+    _write(chunk: Buffer | string, _encoding: string, callback: (err?: Error) => void): void;
     _read(size: number): void;
+    _final(callback: (err?: Error) => void): void;
+    toString(): string;
 }
--- dist/src/lib/StringStream.js
+++ dist/src/lib/StringStream.js
@@ -2,11 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.StringStream = void 0;
 const stream_1 = require("stream");
-class StringStream extends stream_1.Readable {
-    constructor(str) {
-        super();
-        this._string = str;
+class StringStream extends stream_1.Duplex {
+    constructor(str, opts) {
+        super(opts);
+        this._string = str || '';
         this._lastReadBype = 0;
+    }
+    _write(chunk, _encoding, callback) {
+        this._string += chunk instanceof Buffer ? chunk.toString() : chunk;
+        callback();
     }
     _read(size) {
         if (this._lastReadBype >= this._string.length) {
@@ -15,8 +19,15 @@ class StringStream extends stream_1.Readable {
         else {
             const len = Math.min(this._lastReadBype + size, this._string.length);
             this.push(Buffer.from(this._string.substring(this._lastReadBype, len)));
-            this._lastReadBype = size;
+            this._lastReadBype += size;
         }
+    }
+    _final(callback) {
+        callback();
+        this.emit('close');
+    }
+    toString() {
+        return this._string;
     }
 }
 exports.StringStream = StringStream;
